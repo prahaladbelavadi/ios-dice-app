55f196bae0e9decd81eb79a0bf55130626f48393
Prahalad Belavadi <github@belavadi.com>
2020-02-22 01:08:09 +0530

    finish dice making app; to change remote

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    var leftDiceNumber = 0;
+//    var rightDiceNumber = 5;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix");
+//
+//        diceImageView1.alpha = 0.5;
+//
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo");
+//
+//        diceImageView2.alpha = 0.5;
+        
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
 
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ];
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)];
+        diceImageView2.image = diceArray[Int.random(in: 0...5)];
+        
+//        leftDiceNumber = leftDiceNumber + 1;
+//        rightDiceNumber = rightDiceNumber - 1;
+        
+        
+        
+        
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+
+    // print("Button got pressed")
+    
+    }
+    
 }
 
